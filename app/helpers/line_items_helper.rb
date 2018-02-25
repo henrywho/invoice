9a3c4059cdec4da1319209d85e90558dbe1b8f44
Henry Ho <henryho206@gmail.com>
2018-02-24 22:02:55 -0800

    Add whodunnit info to line_item changelog.

--- app/helpers/line_items_helper.rb
+++ app/helpers/line_items_helper.rb
@@ -1,5 +1,11 @@
 module LineItemsHelper
-  def prettify_changeset(changeset)
+  def prettify_changeset(version)
+    updated_by = nil
+    if version.whodunnit
+      updated_by = User.find(version.whodunnit)
+    end
+
+    changeset = version.changeset
     updated_at = changeset.delete("updated_at").second
     summary = "At #{updated_at}, "
 
@@ -7,6 +13,7 @@ module LineItemsHelper
       summary << "#{k} changed from #{v.first} to #{v.second}"
     end
 
+    summary << " by #{updated_by.email}" if updated_by
     summary << "."
 
     summary
